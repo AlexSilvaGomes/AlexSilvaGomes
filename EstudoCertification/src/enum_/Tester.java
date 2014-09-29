e3a915cf98ebe7f2f968bac53ef8b3560732a020
alexsilvagomes@ig.com.br <alexsilvagomes@ig.com.br>
2014-09-29 18:48:13 -0300

    x

--- EstudoCertification/src/enum_/Tester.java
+++ EstudoCertification/src/enum_/Tester.java
@@ -8,5 +8,10 @@ package enum_;
 public class Tester {
 	 public static void main(String [] args) {
 	 System.out.println(Status.Published.index + Status.Rejected.index); // Line 10	 	 
+	 
 	 }
+	 
+	 // Pay attention to this
+	 // it's permitted to put a semicolon (;) it doesn't lead to a compile error.
+	 public void getNumber(){;} // #1
 }
