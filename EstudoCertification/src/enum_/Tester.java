130c85eb8cc8904b69dabb9ec70cd756668ec1b3
alexsilvagomes@ig.com.br <alexsilvagomes@ig.com.br>
2014-09-29 17:57:09 -0300

    teste

--- EstudoCertification/src/enum_/Tester.java
+++ EstudoCertification/src/enum_/Tester.java
@@ -1,6 +1,10 @@
 package enum_;
 
 
+/**
+ * @author ALSGomes
+ *
+ */
 public class Tester {
 	 public static void main(String [] args) {
 	 System.out.println(Status.Published.index + Status.Rejected.index); // Line 10	 	 
