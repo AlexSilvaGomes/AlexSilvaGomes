25959293d44dfdcc5baad50571329e09d2edb0dc
Alex Silva Gomes <alexsilvagomes@ig.com.br>
2014-10-23 22:28:17 -0200

    notes update

--- EstudoCertification/src/test/AuxTest.java
+++ EstudoCertification/src/test/AuxTest.java
@@ -1,19 +1,28 @@
 package test;
 
-import java.util.Formatter;
 
 public  strictfp class AuxTest {
 
 	
 	public static void main(String[] args) {
-		
+        
+		A[] ab = new A[5];
+		ab[0]=new A();
+        
 		boolean b1=false;
 		 boolean b2=true;
 		 Object ob= (b2=false)?((b1=true)?"A":(b1=false)?"B":9800L):((b1=false)?"C":(b2==false)?"D":new java.util.Date());
 		 System.out.print(ob);
+		 
+		 System.out.printf("%1$,+010d",7252);
 		
 		System.out.printf("\nCurrent value is %,+-50.1f",-12300.000);
 		
 	}
 	
 }
+
+
+class A{}
+class B extends A{}
+
