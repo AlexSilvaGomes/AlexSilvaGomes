d24bf6ecd70eb16e2322df6a8635f9dec2451391
alexsilvagomes@ig.com.br <alexsilvagomes@ig.com.br>
2014-10-23 22:20:23 -0200

    from magna

--- EstudoCertification/src/test/AuxTest.java
+++ EstudoCertification/src/test/AuxTest.java
@@ -1,17 +1,15 @@
 package test;
-
-import java.util.Formatter;
-
 public  strictfp class AuxTest {
 
 	
 	public static void main(String[] args) {
 		
+		System.out.print((int)Character.MIN_VALUE+","+(int)Character.MAX_VALUE);
 		boolean b1=false;
-		 boolean b2=true;
-		 Object ob= (b2=false)?((b1=true)?"A":(b1=false)?"B":9800L):((b1=false)?"C":(b2==false)?"D":new java.util.Date());
-		 System.out.print(ob);
-		
+	 boolean b2=true;
+	 Object ob= (b2=false)?((b1=true)?"A":(b1=false)?"B":9800L):((b1=false)?"C":(b2==false)?"D":new java.util.Date());
+	 System.out.print(ob);
+	
 		System.out.printf("\nCurrent value is %,+-50.1f",-12300.000);
 		
 	}
--- EstudoCertification/src/threads/TestThread.java
+++ EstudoCertification/src/threads/TestThread.java
@@ -6,6 +6,7 @@ public class TestThread implements Runnable  {
 	public void run() {
 		try {
 			Thread.sleep(1000);
+			Thread.yield();
 			System.out.println("here");
 		} catch (InterruptedException e) {
 			e.printStackTrace();
@@ -13,9 +14,10 @@ public class TestThread implements Runnable  {
 		
 	}
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		Thread t = new Thread(new TestThread());
 		t.start();
+		
 	}
 
 }
