b85f2afe60f19895cd5996bdb9ff3d1c603eaae5
alexsilvagomes@ig.com.br <alexsilvagomes@ig.com.br>
2014-10-27 16:16:56 -0200

    after merge

--- EstudoCertification/src/test/AuxTest.java
+++ EstudoCertification/src/test/AuxTest.java
@@ -1,10 +1,25 @@
 package test;
-public  strictfp class AuxTest {
+public abstract strictfp class AuxTest {
 
 	
+	 protected abstract void method();
+		
+	 public <E extends B> E getValue(E e){
+		 return e;
+	 }
+	
+	
 	public static void main(String[] args) {
+        
 		
 		System.out.print((int)Character.MIN_VALUE+","+(int)Character.MAX_VALUE);
+		A a = new B();
+		B b = (B) new A();
+		
+		
+		A[] ab = new A[5];
+		ab[0]=new A();
+        
 		boolean b1=false;
 	 boolean b2=true;
 	 Object ob= (b2=false)?((b1=true)?"A":(b1=false)?"B":9800L):((b1=false)?"C":(b2==false)?"D":new java.util.Date());
@@ -15,3 +30,8 @@ public  strictfp class AuxTest {
 	}
 	
 }
+
+
+class A{}
+class B extends A{}
+
