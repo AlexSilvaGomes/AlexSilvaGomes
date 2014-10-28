5c553723e45f3c71fa7f8f7f39f73fe45e20a3f5
Alex Silva Gomes <alexsilvagomes@ig.com.br>
2014-10-27 22:16:40 -0200

    qw

--- EstudoCertification/src/test/AuxTest.java
+++ EstudoCertification/src/test/AuxTest.java
@@ -1,28 +1,24 @@
 package test;
 
-import java.util.NavigableMap;
-import java.util.TreeMap;
 
 
 public abstract strictfp class AuxTest {
 
 	
-	 protected abstract void method();
-		
-	 public <E extends B> E getValue(E e){
-		 return e;
-	 }
-	 
-	 
-	
 	public static void main(String[] args) {
 		
+				A a[] = new B[3];
 				
-	   int[][] it=new int[3][];
-	   it[0]=new int[4];
-       it[1]=new int[6];
-	   it[2]=new int[0];
-	   int [] kt=it[2];
+				a[0] = new C();
+				a[1] = new B();
+				
+				Short i = (short) 129;
+                Short j = (short)129;
+				System.out.println(i==j);				
 	}
-	
 }
+
+
+class A{}
+class B extends A{}
+class C extends B{}
